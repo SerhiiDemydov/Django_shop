98b43f546fe7e25a461ff78725aaa93fec3baabc
SerhiiDemydov <serjdemidov95@gmail.com>
2023-02-10 14:00:56 +0200

    Add first test

--- shop/shop_app/tests.py
+++ shop/shop_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class TestProductView(TestCase):
+
+    def test_product_view(self):
+        response = self.client.get(path=reverse("product"))
+        # assert request.status_code == 200
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode("utf-8"), "Super good")
