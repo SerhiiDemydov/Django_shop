a797e43e673a438e6a74c8650fab53701db3d215
SerhiiDemydov <serjdemidov95@gmail.com>
2023-02-13 16:22:13 +0200

    Add PostgreSQL DataBase

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -82,11 +82,16 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_shop",
+        "USER": "django",
+        "PASSWORD": "django",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
+
 
 
 # Password validation
